84f602deb46ff7d186b4235f5026106a3bdc68dd
AndreaMainardi123 <114904170+AndreaMainardi123@users.noreply.github.com>
2023-02-27 10:30:36 +0100

    update

--- news/urls.py
+++ news/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import home, ArticoloDetailViewCB,ArticoloListView,GiornalistaDetailViewCB,GiornalistaListView 
+from .views import home, ArticoloDetailViewCB,ArticoloListView,GiornalistaDetailViewCB,GiornalistaListView,giornalisti_list_api,giornalisti_api
 
 app_name="news"
 
@@ -9,4 +9,6 @@ urlpatterns = [
     path('lista_articoli/',ArticoloListView.as_view(),name="lista_articoli"),
     path('giornalisti/<int:pk>',GiornalistaDetailViewCB.as_view(),name="giornalista_detail"),
     path('lista_giornalisti/',GiornalistaListView.as_view(),name="lista_giornalisti"),
-]
+    path('lista_giornalisti_api/',giornalisti_list_api,name="giornalisti_list_api"),
+    path('giornalisti_api/<int:pk>',giornalisti_api,name="giornalisti_api"),
+    ]
